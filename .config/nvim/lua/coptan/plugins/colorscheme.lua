9048dc8703c063fab0b46c6ea8db38d2af8bbde8
Omar Mohamed <omarcoptan9@gmail.com>
2023-12-17 23:58:14 +0000

    nvim: colorscheme: Disable background

--- .config/nvim/lua/coptan/plugins/colorscheme.lua
+++ .config/nvim/lua/coptan/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
     "rose-pine/neovim",
     priority = 1000,
     config = function()
+        require('rose-pine').setup({
+            disable_background = true,
+        })
         vim.cmd([[colorscheme rose-pine]])
     end,
 }
