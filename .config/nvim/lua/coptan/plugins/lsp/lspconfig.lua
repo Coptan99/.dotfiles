df9b4a6da37c020fcda0497580a496f23fcb74d6
Omar Mohamed <omarcoptan9@gmail.com>
2023-12-29 09:24:18 +0000

    Edit nvim lsp keybindings and add tsserver

--- .config/nvim/lua/coptan/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/coptan/plugins/lsp/lspconfig.lua
@@ -18,44 +18,16 @@ return {
             opts.buffer = bufnr
 
             -- set keybinds
-            opts.desc = "Show LSP references"
-            keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-            opts.desc = "Go to declaration"
-            keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-            opts.desc = "Show LSP definitions"
-            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-            opts.desc = "Show LSP implementations"
-            keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-            opts.desc = "Show LSP type definitions"
-            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-            opts.desc = "See available code actions"
-            keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-            opts.desc = "Smart rename"
-            keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-            opts.desc = "Show buffer diagnostics"
-            keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-            opts.desc = "Show line diagnostics"
-            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-            opts.desc = "Go to previous diagnostic"
-            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-            opts.desc = "Go to next diagnostic"
-            keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-            opts.desc = "Show documentation for what is under cursor"
-            keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-            opts.desc = "Restart LSP"
-            keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts) -- show documentation for what is under cursor
+            keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts) -- show all symbols in current buffer
+            keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts) -- show diagnostics for line
+            keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts) -- jump to next diagnostic in buffer
+            keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts) -- jump to previous diagnostic in buffer
+            keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts) -- see available code actions, in visual mode will apply to selection
+            keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts) -- shw definition, references
+            keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts) -- smart rename
+            keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts) -- show signature help
         end
 
         -- used to enable autocompletion (assign to every lsp server config)
@@ -94,6 +66,11 @@ return {
 
         -- configure python server
         lspconfig["pylsp"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
+        lspconfig["tsserver"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
