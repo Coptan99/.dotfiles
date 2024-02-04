a2ddf2d5f81a234e210a06852db6a0dc08103e80
Omar Mohamed <omarcoptan9@gmail.com>
2024-02-05 00:15:44 +0200

    scripts: exit.sh: Fix the script

--- .local/bin/exit.sh
+++ .local/bin/exit.sh
@@ -2,6 +2,10 @@
 
 choice=$(echo "Shutdown\nReboot\nExit" | dmenu -i -p "Select an action: ")
 
-[[ -d $choice == "Shutdown" ]] && doas poweroff
-[[ -d $choice == "Reboot" ]] && doas reboot
-[[ -d $choice == "Exit" ]] && pkill dwm
+if [[ "$choice" == "Shutdown" ]]; then
+    doas poweroff
+elif [[ "$choice" == "Reboot" ]]; then
+    doas reboot
+elif [[ "$choice" == "Exit" ]]; then
+    pkill dwm
+fi
