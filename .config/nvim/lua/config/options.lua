ae38218f06178f6c4a9d1bc034d58510a233c084
Omar Mohamed <omarcoptan9@gmail.com>
2024-12-21 11:14:51 +0200

    config/nvim: rearrange options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,35 +2,29 @@ vim.opt.exrc = true
 vim.opt.mouse = ""
 vim.opt.guicursor = ""
 vim.opt.updatetime = 50
+vim.opt.background = "dark"
 vim.opt.termguicolors = true
 vim.opt.isfname:append("@-@")
 vim.opt.shortmess:append({ I = true })
 
-vim.opt.background = "dark"
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
-
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
 vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
