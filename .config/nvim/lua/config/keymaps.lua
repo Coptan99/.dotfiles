e19bbc6d4a28c7cb786e1ac7d1b44531c7f8fe12
Omar Mohamed <omarcoptan9@gmail.com>
2024-12-21 11:52:37 +0200

    config/nvim: refactor keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>ee", ":Oil<cr>")
-
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
@@ -21,7 +19,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>cprev<cr>zz")
 vim.keymap.set("n", "<leader>K", "<cmd>lnext<cr>zz")
 vim.keymap.set("n", "<leader>J", "<cmd>lprev<cr>zz")
 
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", ":Ex<cr>")
 vim.keymap.set("i", "<C-c>", "<esc>")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
