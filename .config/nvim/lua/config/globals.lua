302fa62b3ff70f5b0e5c1da2b34f3a1aa7e44612
Omar Mohamed <omarcoptan9@gmail.com>
2024-12-21 11:39:29 +0200

    config/nvim: define global variables for python

--- .config/nvim/lua/config/globals.lua
+++ .config/nvim/lua/config/globals.lua
@@ -3,3 +3,7 @@ vim.g.netrw_browse_split = false
 vim.g.netrw_altv = true
 vim.g.netrw_winsize = 25
 vim.g.netrw_fastbrowse = false
+
+vim.g.python_host_skip_check = true
+vim.g.python3_host_skip_check = true
+vim.g.loaded_python3_provider = false
